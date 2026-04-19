34cba336d619338e41a596e0d4e23577ed59c84f
092wb <you@example.com>
2026-04-19 20:15:53 +0800

    update

--- src/client/components/FilesPageMain.vue
+++ src/client/components/FilesPageMain.vue
@@ -28,7 +28,7 @@ const content = computed(()=>frontmatter.value.flistData?.content);
       <FContent v-if="content" :content="content" style="padding: 1rem"></FContent>
     </div>
     <div class="footer">
-      <FLink to="https://github.com/jianjianai/FList">由 FList 强力驱动</FLink>
+      <FLink to="https://github.com/092wb/092wb">092Team By 往风</FLink>
     </div>
 <!--    消息弹出框-->
     <Notification></Notification>
